be25ef42e3fe0154805f66eb1bfcdcb38ae48a4d
dkeatmobile-droid <dkeatmobile@gmail.com>
2026-02-20 17:45:32 +0000

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,18 @@
+const CACHE = "wtd-tracker-v2";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k)))))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).catch(() => cached))
+  );
+});
